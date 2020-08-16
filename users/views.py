552c679d4d98d89a49e9b47643776008f6a82335
Shaiene <shaiene-kun@pop-os.localdomain>
2020-08-16 20:36:47 -0300

    Created basic createView class

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+#from django.auth.
 # Create your views here.
+
+class CreateUser(CreateView):
+    template_name = 'users/create.html'
+    model = User
+    fields = ['username', 'email', 'password']
+
+
