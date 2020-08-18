e75359d655b60b572e9e8c9a0538d786af02a514
Yasmin Libano <min@pop-os.localdomain>
2020-08-18 00:08:10 -0300

    Organizing file

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 
 # Class views
@@ -14,14 +14,13 @@ class ArticleView(DetailView):
 class NewPostView(CreateView):
     model = Post
     template_name = 'posts/postForm.html'
-    fields = ["title","author","body"]
+    fields = ('title', 'author', 'body',)
 
 
+
+#Function Views
 def base(request):
     return render(request,'base/base.html',{})
 
-# Create your views here.
-
-# Function Views
 # def home(request):
 #     return render(request,'home.html', {})
