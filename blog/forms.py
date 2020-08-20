10291445f2f1b0c6b5a03cecd84edf6236c5ae1a
Yasmin Libano <min@pop-os.localdomain>
2020-08-19 21:34:36 -0300

    Getting rid of non-used content

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,7 @@
 from django import forms
 from .models import Post
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 
 class PostForm(forms.ModelForm):
-    
     class Meta:
         model = Post
         fields = ("title","author", "body")
