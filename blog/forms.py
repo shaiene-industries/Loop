45e42b57054334b5a33a46b323785acce4ebab45
Yasmin Libano <min@pop-os.localdomain>
2020-08-23 22:05:29 -0300

    Styling PostForm

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 from .models import Post
+from markdownx.fields import MarkdownxFormField
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ("title","author", "body")
+        fields = ("title","author", "body",)
+        
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'author':forms.Select(attrs={'class':'form-control'}),
+            'body': MarkdownxFormField(),
+        }
 
+        labels = {
+            'title': ('Title*'),
+            'author': ('Author*'),
+        }
         
