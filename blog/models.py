fc190c6ec0369ecfdcefdf59693ac2eeaed31b2f
Yasmin Libano <min@pop-os.localdomain>
2020-08-23 22:05:54 -0300

    Changing current model to inject markdown in post.body

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from markdownx.models import MarkdownxField
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    body = MarkdownxField()
 
     def __str__(self):
         return self.title + " | by " + str(self.author)
 
     def get_absolute_url(self):
         return reverse('blog:articleDetail', kwargs={'pk': str(self.id)})
+    
     
     
 
