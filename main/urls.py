fceff5bd11ae0d538b3512926c419c1a558d2f44
Shaiene <shaiene-kun@pop-os.localdomain>
2020-09-14 22:54:15 -0300

    Corrigindo urls

--- main/urls.py
+++ main/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('blog.urls',namespace='blog') ),
     path('', include('index.urls',namespace='index')),
-    url(r'^markdownx/', include(markdownx))
-    path('users/', include('users.urls',namespace='users'))
+    url(r'^markdownx/', include(markdownx)),
+    path('users/', include('users.urls',namespace='users')),
 ]
 
