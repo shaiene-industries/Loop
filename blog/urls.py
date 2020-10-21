5b07019a36040fa179c4a817c7e631622f027314
Shaiene <shaiene-kun@pop-os.localdomain>
2020-10-20 21:32:46 -0300

    Adding remove post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from . import views # Use this for functions
-from .views import PostsView, ArticleView, NewPostView, UpdatePostView # Use this for classes
+from .views import PostsView, ArticleView, NewPostView, UpdatePostView, DeletePostView # Use this for classes
 from django.contrib.auth.views import LoginView
 
 app_name = 'blog'
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', PostsView.as_view(), name = "posts"),
     path('article/<int:pk>', ArticleView.as_view(), name='articleDetail'),
     path('article/<int:pk>/update', UpdatePostView.as_view(), name='articleUpdate'),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name='articleDelete'),
     path('postForm', NewPostView.as_view(), name='postForm'),
     path('accounts/login/', LoginView.as_view(template_name='auth/login.html'), name='login')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import PostForm
@@ -9,25 +9,29 @@ from .forms import PostForm
 # Class views
 
 # See docs for ListView
+# Lists all posts
 class PostsView(ListView):
     model = Post
     template_name = "posts/home.html"
 
 # See docs for DetailView
+# Show the article in full detail
 class ArticleView(DetailView):
     model = Post
     template_name = "posts/articleDetail.html"
 
 # See docs for LoginRequiredMixin, UpdateView and reverse_lazy
+# Allows user to change the content of the article
 class UpdatePostView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'posts/postForm.html'
     # form_class = PostForm
     login_url = reverse_lazy('blog:login')
     fields = ['title','body']
-    success_url = "/posts"
+    success_url = reverse_lazy('blog:posts')
 
 # See docs for LoginRequiredMixin and CreateView
+# Allows user to create a new Article
 class NewPostView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'posts/postForm.html'
@@ -39,6 +43,10 @@ class NewPostView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class DeletePostView(LoginRequiredMixin, DeleteView):
+    model = Post
+    login_url = reverse_lazy('blog:login')
+    success_url = reverse_lazy('blog:posts')
 
 #Function Views
 # def base(request):
