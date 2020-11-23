172e0a51ebbe29e304863ece069d39075b835573
Shaiene <shaiene-kun@pop-os.localdomain>
2020-11-22 23:56:45 -0300

    Adding profile to user admin edit page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 # Register your models here.
+from .models import Profile
+
+class ProfileInline(admin.TabularInline):
+    """
+    Shows profile form inside the user admin form
+    """
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profile"
+    fk_name = "user"
+
+class CustomUserAdmin(UserAdmin):
+    """
+    "Overriding User admin page, to add profile as an Inline"
+    """
+    inlines = (ProfileInline, )
+    
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
