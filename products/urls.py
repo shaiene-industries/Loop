d7a31381d6f2a2061e5be0d7c65f89e227775233
Yasmin <flor.yasmin90@gmail.com>
2021-03-03 22:55:21 -0300

    produtos e user

--- products/urls.py
+++ products/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
     # Páginas dos produtos
     path('products/add', NewProductView.as_view(), name='addProduct'),
-    path('products/<int:pk>/info', ProductView.as_view(), name='productDetail'),
+    path('products/<int:user_pk>/<int:pk>/info', ProductView.as_view(), name='productDetail'),
     path('products/<int:pk>/update', UpdateProductView.as_view(), name='productUpdate'),
     path('products/<int:pk>/delete', DeleteProductView.as_view(), name='productDelete'),
 ]
