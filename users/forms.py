0f40b165690a03a13d6a2b54d0c5bc6e76b9a28e
Yasmin <flor.yasmin90@gmail.com>
2021-03-11 19:56:04 -0300

    Movendo bootstrap_format para arquivo separado

--- users/forms.py
+++ users/forms.py
@@ -1,40 +1,8 @@
+from utils.form_utils import *
 from django import forms
 from django.forms import widgets
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
-def bootstrap_format(fields : dict, float=False):    
-    """
-    Aplica a classe .form-control nos inputs e o tipo 'date' nos inputs de data
-    """
-    NON_FORM_CONTROL = ('hidden', 'checkbox')
-    def add_widget_attrs(field, value, attr="class"):
-        try:
-            field.widget.attrs[attr] += " "+value
-        except KeyError:
-            field.widget.attrs[attr] = value
-        return field
-    for field in fields:
-        try:
-            ### Formatação de inputs
-            # form-control
-            if (fields[field].widget.input_type not in NON_FORM_CONTROL):        
-                fields[field] = add_widget_attrs(fields[field], "form-control")
-                if float:
-                    fields[field] = add_widget_attrs(fields[field], "placeholder", "placeholder")
-            # date
-            if ('data' in field):
-                fields[field].widget.attrs['title'] = "Insira uma data no formato dd/mm/aaaa"
-                fields[field].widget.input_type = "date"
-            # Interrogação para checkbox
-            if (fields[field].widget.input_type == 'checkbox'):
-                fields[field] = add_widget_attrs(fields[field], "check-question")
-            # Seta em selects
-            if (fields[field].widget.input_type == 'select'):
-                fields[field] = add_widget_attrs(fields[field], "form-select")
-        except AttributeError:            
-            continue
-    return fields
 
 class LoginForm(AuthenticationForm):
     def __init__(self, request=None, *args, **kwargs):
