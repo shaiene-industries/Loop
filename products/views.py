86479c3a68419231b844a01771332488621f56d3
Yasmin <flor.yasmin90@gmail.com>
2021-03-14 05:41:22 -0300

    Melhorias e consertos diversos

--- products/views.py
+++ products/views.py
@@ -21,7 +21,7 @@ class FeedView(ListView):
     model = Products 
     template_name = "products/feed.html"
     context_object_name = 'products'
-    paginate_by = 2
+    paginate_by = 5
     queryset = Products.objects.all().order_by('created_at')
 
 class ProductView(DetailView):
@@ -64,8 +64,11 @@ def exchange(request,pk):
     page with exchange form. Also saves form.
     """
     product_chosen = get_object_or_404(Products,pk=pk) 
+    user_chosen= product_chosen.user
+    saved = False
 
     if request.method == 'GET':
+        form = ExchangeForm(initial={'product_chosen':product_chosen}, user=request.user)
         # Is the product mine?
         if product_chosen.user == request.user:
             raise PermissionDenied("Esse item é seu. Como você vai trocar consigo mesmo? O.o")
@@ -74,27 +77,20 @@ def exchange(request,pk):
             raise PermissionDenied("Desculpe, esse item já foi trocado. Quer tentar olhar outros")
         #  Is the product being exchanged?
         if Troca.objects.filter(Q(product_chosen=product_chosen) | Q(product_to_exchange=product_chosen)).exists():
-            raise PermissionDenied("Desculpe, esse produto está em processo de troca, caso ele a troca não seja efetuada você pode tentar novamente")
+            raise PermissionDenied("Desculpe, esse produto está em processo de troca, caso a troca não seja efetuada você pode tentar novamente")
     elif request.method == "POST":
-        form = ExchangeForm(request.POST)
+        form = ExchangeForm(request.POST, initial={'product_chosen':product_chosen}, user=request.user)
         if form.is_valid():
             form.save()
+            saved = True
     
-    form = ExchangeForm(product_chosen=product_chosen, user=request.user)
-    user_product = product_chosen.user
-
-        
     return render(
         request,
         'products/exchange.html',
         {
             'form': form, 
-            'user': user_product, 
-            'product': product_chosen, 
+            'user': user_chosen, 
+            'product_chosen': product_chosen, 
+            'saved':saved
         }        
     )
-
-
-
-# Esse item é seu. Como você vai trocar consigo mesmo? O.o
-# Desculpe, esse item já foi trocado. Quer tentar olhar outros
