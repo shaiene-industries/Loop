de6dbb06c4af5b01a6be2eac34c6e75e9c2e8ae9
Yasmin <flor.yasmin90@gmail.com>
2021-03-22 13:03:02 -0300

    Removendo bibliotecas não utilizadas

--- main/settings.py
+++ main/settings.py
@@ -45,14 +45,7 @@ INSTALLED_APPS = [
     'sass_processor',
     'utils',
     'django_select2',
-    'easy_thumbnails',
-    'image_cropping',
 ]
-
-from easy_thumbnails.conf import Settings as thumbnail_settings
-THUMBNAIL_PROCESSORS = (
-    'image_cropping.thumbnail_processors.crop_corners',
-) + thumbnail_settings.THUMBNAIL_PROCESSORS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
